cfd995ac5eb00c375631ebc11cff90acaf27b7b1
mx_in <mouxin1993@gmail.com>
2022-07-03 01:06:56 +0800

    feat: about and nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><Link href="/"><a>Home</a></Link></li>
+          <li><Link href="/"><a>About</a></Link></li>
+        </ul>
+      </nav>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp

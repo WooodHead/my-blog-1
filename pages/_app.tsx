95933a0f43d5aa0a35acdf0c328e517347cc2421
mx_in <mouxin1993@gmail.com>
2022-07-07 01:01:56 +0800

    feat: make home layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,16 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/about"><a>About</a></Link></li>
-        </ul>
-      </nav>
-
+    <div className="mx-auto my-8 w-9/12" >
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Blog</h1>
+        <nav className="my-4">
+          <ul className="flex flex-row space-x-4 justify-center">
+            <li><Link href="/"><a className="text-blue-600 hover:text-blue-500">Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+          </ul>
+        </nav>
+      </header>
       <Component {...pageProps} />
     </div>
   )
